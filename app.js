e19dcb16150f1a50941e839bef4327a00bf6db94
Matt Winchester <m_winche@yahoo.com>
2014-05-27 13:11:53 -0600

    now we're cooking with gas! (custom elements)

--- app.js
+++ app.js
@@ -10,15 +10,27 @@
 		gender_male = $('gender_male'),
 		gender_female = $('gender_female');
 
-	lifeButton.addEventListener('click', function(){
+
+	//Create custom at-person element
+	var proto = Object.create(HTMLElement.prototype);
+
+	proto.createdCallback = function(){
 		var clone = personTmpl.content.cloneNode(true);
 
 		clone.querySelector('.name').innerText = name.value;
 		clone.querySelector('.age').innerText = age.value;
 		clone.querySelector('.gender').innerText = gender_male.checked ? gender_male.value : gender_female.checked ? gender_female.value : '';
 
+		this.appendChild(clone);
+	};
 
-		peopleList.insertBefore(clone, peopleList.firstChild);
+	var PersonElement = document.registerElement('at-person', {
+		prototype: proto
+	});
+
+	//Click handler just creates a new at-person element
+	lifeButton.addEventListener('click', function(){
+		peopleList.insertBefore(new PersonElement(), peopleList.firstChild);
 	});
 
 })(document.getElementById.bind(document));
