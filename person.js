aee8e84dfb1b742ced980412b59d4c620a866de3
Matt Winchester <m_winche@yahoo.com>
2014-05-27 19:49:19 -0600

    abstract away boilerplate

--- person.js
+++ person.js
@@ -10,17 +10,21 @@
 		gender_female = $('gender_female');
 
 
-	document.registerElement('at-person', {
-		prototype: _.assign(Object.create(HTMLElement.prototype), {
-			createdCallback: function(){
-				var clone = personTmpl.content.cloneNode(true);
+	function customElement(elementName, overrides){
+		return document.registerElement(elementName, {
+			prototype: _.assign(Object.create(HTMLElement.prototype), overrides)
+		});
+	}
 
-				clone.querySelector('.name').innerText = name.value;
-				clone.querySelector('.age').innerText = age.value;
-				clone.querySelector('.gender').innerText = gender_male.checked ? gender_male.value : gender_female.checked ? gender_female.value : '';
+	customElement('at-person', {
+		createdCallback: function(){
+			var clone = personTmpl.content.cloneNode(true);
 
-				this.createShadowRoot().appendChild(clone);
-			}
-		})
+			clone.querySelector('.name').innerText = name.value;
+			clone.querySelector('.age').innerText = age.value;
+			clone.querySelector('.gender').innerText = gender_male.checked ? gender_male.value : gender_female.checked ? gender_female.value : '';
+
+			this.createShadowRoot().appendChild(clone);
+		}
 	});
 })(document.getElementById.bind(document));
