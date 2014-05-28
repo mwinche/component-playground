b98c045a3f875a168b7db13b5bf428bac3a34465
Matt Winchester <m_winche@yahoo.com>
2014-05-27 19:48:02 -0600

    spiced things up with lodash

--- person.js
+++ person.js
@@ -1,32 +1,26 @@
 (function($){
 	"use strict";
 
-	var importDoc = document.currentScript.ownerDocument;
-	
-	var personTmpl = importDoc.getElementById('person');
+	var importDoc = document.currentScript.ownerDocument,
+		personTmpl = importDoc.getElementById('person'),
 
-	var name = $('name'),
+		name = $('name'),
 		age = $('age'),
 		gender_male = $('gender_male'),
 		gender_female = $('gender_female');
 
 
-	//Create custom at-person element
-	var proto = Object.create(HTMLElement.prototype);
+	document.registerElement('at-person', {
+		prototype: _.assign(Object.create(HTMLElement.prototype), {
+			createdCallback: function(){
+				var clone = personTmpl.content.cloneNode(true);
 
-	proto.createdCallback = function(){
-		var clone = personTmpl.content.cloneNode(true);
+				clone.querySelector('.name').innerText = name.value;
+				clone.querySelector('.age').innerText = age.value;
+				clone.querySelector('.gender').innerText = gender_male.checked ? gender_male.value : gender_female.checked ? gender_female.value : '';
 
-		clone.querySelector('.name').innerText = name.value;
-		clone.querySelector('.age').innerText = age.value;
-		clone.querySelector('.gender').innerText = gender_male.checked ? gender_male.value : gender_female.checked ? gender_female.value : '';
-
-		var shadow = this.createShadowRoot();
-
-		shadow.appendChild(clone);
-	};
-
-	var PersonElement = document.registerElement('at-person', {
-		prototype: proto
+				this.createShadowRoot().appendChild(clone);
+			}
+		})
 	});
 })(document.getElementById.bind(document));
